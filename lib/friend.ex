e3d9c480c8f38ca6625de7684d97146b425bb005
Steve Grossi <me@stevegrossi.com>
2016-12-06 06:08:57 -0500

    GenServer friend with a nice, clean API

--- lib/friend.ex
+++ lib/friend.ex
@@ -5,6 +5,16 @@ defmodule Friend do
     GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   end
 
+  def set(key, value) do
+    GenServer.cast(__MODULE__, {:set, key, value})
+  end
+
+  def get(key) do
+    GenServer.call(__MODULE__, {:get, key})
+  end
+
+  # Callbacks
+
   def handle_cast({:set, key, value}, state) do
     new_state = Map.put(state, key, value)
     {:noreply, new_state}
