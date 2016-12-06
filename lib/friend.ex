17478dda42c6e0cfe2ec808708eec6d9762ace83
Steve Grossi <me@stevegrossi.com>
2016-12-06 06:08:40 -0500

    Simple GenServer friend

--- lib/friend.ex
+++ lib/friend.ex
@@ -1,15 +1,17 @@
 defmodule Friend do
+  use GenServer
 
-  def listen(state \\ %{}) do
-    receive do
-      {:set, key, value} ->
-        new_state = Map.put(state, key, value)
-        listen(new_state)
-      {:get, key} ->
-        IO.puts(Map.get(state, key))
-        listen(state)
-      _ ->
-        nil
-    end
+  def start_link do
+    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+  end
+
+  def handle_cast({:set, key, value}, state) do
+    new_state = Map.put(state, key, value)
+    {:noreply, new_state}
+  end
+
+  def handle_call({:get, key}, _sender, state) do
+    response = Map.get(state, key)
+    {:reply, response, state}
   end
 end
