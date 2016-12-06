c04157ac6ca5dc2d948bae38b015ee0cb5439ba5
Steve Grossi <me@stevegrossi.com>
2016-12-06 15:10:03 -0500

    Connect Client to Store

--- lib/reminders/client.ex
+++ lib/reminders/client.ex
@@ -1,24 +1,37 @@
 defmodule Reminders.Client do
   use GenServer
+  alias Reminders.Store
 
   def start_link do
     GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def init(state) do
+    run_clock()
+    {:ok, state}
   end
 
   def remind_me(message, seconds_in_future) do
     GenServer.cast(__MODULE__, {:new, message, seconds_in_future})
   end
 
+  defp run_clock do
+    Process.send_after(self(), :tick, :timer.seconds(1))
+  end
+
   # Callbacks
 
   def handle_cast({:new, message, seconds_in_future}, state) do
-    milliseconds = seconds_in_future * 1000
-    Process.send_after(self(), {:reminder, message}, milliseconds)
+    remind_at = :os.system_time(:seconds) + seconds_in_future
+    Store.save({message, remind_at})
     {:noreply, state}
   end
 
-  def handle_info({:reminder, message}, state) do
-    IO.puts("REMINDER: #{message}")
+  def handle_info(:tick, state) do
+    Enum.each(Store.pop_past_reminders, fn({message, _timestamp}) ->
+      IO.puts("REMINDER: #{message}")
+    end)
+    run_clock()
     {:noreply, state}
   end
 
